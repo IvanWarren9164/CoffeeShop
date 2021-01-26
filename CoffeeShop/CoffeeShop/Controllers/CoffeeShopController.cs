7381895571998decbad746517d47f7ee3287285c
Ivan <iwarren9164@gmail.com>
2021-01-26 17:31:36 -0500

    Coffee Shop Submission

---
+++ CoffeeShop/CoffeeShop/Controllers/CoffeeShopController.cs
@@ -0,0 +1,34 @@
+﻿using CoffeeShop.Models.CoffeeShop;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            var viewModel = new RegisterViewModel();
+
+
+            return View(viewModel);
+        }
+        public IActionResult DisplayInfo(RegisterViewModel model)
+        {
+
+            var displayModel = new DisplayViewModel();
+
+            displayModel.DisplayName = model.FirstName;
+
+            return View("Display", displayModel);
+        }
+    }
+}
---
+++ CoffeeShop/CoffeeShop/Models/CoffeeShop/DisplayViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models.CoffeeShop
+{
+    public class DisplayViewModel
+    {
+        public string DisplayName { get; set; }
+    }
+}
